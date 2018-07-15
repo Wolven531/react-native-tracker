b9ff7f6b2425a50b65e74ccb9ed15a95eb159fdb
Anthony Williams <anthony.williams.cs@gmail.com>
2018-07-15 16:06:08 -0600

    first attempt at testing with jest

---
+++ tests/HomeScreen-test.js
@@ -0,0 +1,15 @@
+import 'react-native'
+import React from 'react'
+import 'jest'
+// import renderer from 'react-test-renderer'
+
+// import { configure } from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+
+// configure({ adapter: new Adapter() })
+
+import { HomeScreen } from '../components/HomeScreen'
+
+test('some dummy test', () => {
+	expect(1 + 4).toBe(5)
+})
